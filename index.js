b2013747d11b85bdf50d72c7a9601e0d51032086
AkhileshTalekar <akhilesh.talekar21@gmail.com>
2025-11-18 19:49:16 +0530

    chore: initialize package.json with dependencies for express, cors, and nodemon

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"], // Add all frontend URLs explicitly
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+    ],
+  })
+);
+
+app.use(bodyParser.json());
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/data", (req, res) => {
+  res.json({ message: "This is some data from the backend!" });
+});
